b779d085b174dd5ba5c72ce56292ded234fc8403
shubham <rathoreshubham706@gmail.com>
2023-01-18 14:12:11 +0530

    template

--- agile-soup-311/src/main/java/com/masai/Exception/GlobleExceptionHandler.java
+++ agile-soup-311/src/main/java/com/masai/Exception/GlobleExceptionHandler.java
@@ -1,5 +1,16 @@
 package com.masai.Exception;
 
-public class GlobleExceptionHandler {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
+@ControllerAdvice
+public class GlobleExceptionHandler {
+	
+	@ExceptionHandler(LoginException.class)
+	public ResponseEntity<MyError> LoginExceptionHandler(LoginException msg,WebRequest request){
+		
+	}
+	
 }
