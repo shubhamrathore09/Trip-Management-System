318db2ea9d59d6f6e7c25ac5ddc85ea1579eed6c
shubham <rathoreshubham706@gmail.com>
2023-01-18 09:56:48 +0530

    added

---
+++ agile-soup-311/src/main/java/com/masai/AgileSoup311Application.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileSoup311Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileSoup311Application.class, args);
+	}
+
+}
