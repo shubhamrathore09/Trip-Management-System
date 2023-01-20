dfc50bbeed4a5db88488a03935e0c0ffbd5808a9
imsaksham <sakshamkumarsingh99@gmail.com>
2023-01-20 11:51:05 +0530

    added

---
+++ agile-soup-311/src/main/java/com/masai/Exception/HotelException.java
@@ -0,0 +1,11 @@
+package com.masai.Exception;
+
+public class HotelException extends RuntimeException {
+
+	public HotelException() {
+	}
+
+	public HotelException(String message) {
+		super(message);
+	}
+}
