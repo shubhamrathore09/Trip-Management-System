dd2650863c7b3a37b1da054140e7313174553244
Kajol1106 <kolagirkajol@gmail.com>
2023-01-20 22:03:17 +0530

    day-4

--- agile-soup-311/src/main/java/com/masai/ServiceImpl/PackageServiceImpl.java
+++ agile-soup-311/src/main/java/com/masai/ServiceImpl/PackageServiceImpl.java
@@ -50,39 +50,4 @@ public class PackageServiceImpl implements PackageService{
 			throw new PackageException("Package list is empty...");
 		return list;
 	}
-
-//	@Override
-//	public Package addPackage(Package p) throws PackageException {
-//		Package pk = packageRepo.save(p);
-//		if(pk == null)
-//			throw new PackageException("Package is not saved...Please provide correct details");
-//		return pk;
-//	}
-//
-//	@Override
-//	public Package deletePackage(Integer id) throws PackageException {
-//		Optional<Package> opt = packageRepo.findById(id);
-//		if(opt.isEmpty())
-//			throw new PackageException("Package is not found with Id : "+id +" Please provide correct id.");
-//		Package p = opt.get();
-//		packageRepo.delete(p);
-//		return p;
-//	}
-//
-//	@Override
-//	public Package searchPackage(Integer id) throws PackageException {
-//		Optional<Package> opt = packageRepo.findById(id);
-//		if(opt.isEmpty())
-//			throw new PackageException("Package is not found with Id : "+id +" Please provide correct id.");
-//		return opt.get();
-//	}
-//
-//	@Override
-//	public List<Package> viewAllPackages() throws PackageException {
-//		List<Package> list = packageRepo.findAll();
-//		if(list.isEmpty())
-//			throw new PackageException("Package list is empty...");
-//		return list;
-//	}
-
 }
