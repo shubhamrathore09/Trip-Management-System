a92fe4efd58dd1aa6a6e0d0963ce98592fcdbcbc
Kajol1106 <kolagirkajol@gmail.com>
2023-01-20 20:12:22 +0530

    day 4

--- agile-soup-311/src/main/java/com/masai/ServiceImpl/BookingServiceImpl.java
+++ agile-soup-311/src/main/java/com/masai/ServiceImpl/BookingServiceImpl.java
@@ -1,8 +1,12 @@
 package com.masai.ServiceImpl;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.masai.Exception.BookingException;
 import com.masai.Repository.BookingRepository;
 import com.masai.Service.BookingService;
 import com.masai.model.Booking;
@@ -14,8 +18,37 @@ public class BookingServiceImpl implements BookingService{
 	private BookingRepository bookingRepo;
 
 	@Override
-	public Booking makeBooking(Booking booking) {
-		return bookingRepo.save(booking);
+	public Booking makeBooking(Booking booking) throws BookingException {
+		Booking b = bookingRepo.save(booking);
+		if(b == null)
+			throw new BookingException("Not able to save booking...");
+		return b;
+	}
+
+	@Override
+	public Booking cancelBooking(Integer id) throws BookingException {
+		Optional<Booking> opt = bookingRepo.findById(id);
+		if(opt.isEmpty())
+			throw new BookingException("Booking is not found with the ID : "+ id + " Please provide correct ID.");
+		Booking b = opt.get();
+		bookingRepo.delete(b);
+		return b;
+	}
+
+	@Override
+	public Booking viewBooking(Integer id) throws BookingException {
+		Optional<Booking> opt = bookingRepo.findById(id);
+		if(opt.isEmpty())
+			throw new BookingException("Booking is not found with the ID : "+ id + " Please provide correct ID.");
+		return opt.get();
+	}
+
+	@Override
+	public List<Booking> viewAllBooking() throws BookingException {
+		List<Booking> list = bookingRepo.findAll();
+		if(list.isEmpty())
+			throw new BookingException("Booking list is empty...");
+		return list;
 	}
 
 }
