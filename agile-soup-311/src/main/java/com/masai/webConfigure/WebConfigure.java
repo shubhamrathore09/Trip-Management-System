1780923471a16292cd5d98529cffd9a7c0c78728
Rocker Ni3 <nitin939426@gmail.com>
2023-01-31 22:18:39 +0530

    added

---
+++ agile-soup-311/src/main/java/com/masai/webConfigure/WebConfigure.java
@@ -0,0 +1,15 @@
+package com.masai.webConfigure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+@EnableWebMvc
+public class WebConfigure implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry corsRegistry) {
+		corsRegistry.addMapping("/**").allowedOriginPatterns("*").allowedMethods("*").maxAge(3600L).allowedHeaders("*")
+				.exposedHeaders("Authorization").allowCredentials(true);
+	}
+}
